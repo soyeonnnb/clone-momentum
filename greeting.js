abff80227973ad092ad002b1e7b9964e882c9a89
soyeonnnb <soyeonnnb@users.noreply.github.com>
2021-01-20 02:57:25 +0900

    added-some-object
    This is my very first js project. This is so difficult for me that upload is slowly..

--- greeting.js
+++ greeting.js
@@ -5,14 +5,31 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
+
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
